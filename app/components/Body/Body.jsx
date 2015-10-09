91b7363e967a103c1f03b87d3ab061329f82087e
David Vella <dave@deepcobalt.com>
2015-10-10 01:53:55 +0200

    Adding description

--- app/components/Body/Body.jsx
+++ app/components/Body/Body.jsx
@@ -50,9 +50,13 @@ export default class Body extends React.Component {
       display: this.props.time > 0 || this.state.hasStarted ? 'none' : 'block'
     };
 
-    var trackInfoStyle = {
-      opacity: this.props.track.title ? 0.5 : 0
-    };
+    var highlightStyle = {
+      color: this.props.colourScheme[4]
+    }
+
+    // var trackInfoStyle = {
+    //   opacity: this.props.track.title ? 0.5 : 0
+    // };
 
     return (
       <div className={styles.body}>
@@ -63,17 +67,28 @@ export default class Body extends React.Component {
         <TweetWall {...this.props}/>
 
         <div className={styles.intro} style={{ display: this.state.hasStarted == true ? 'none' : 'block' }}>
-          <p>On July 29th 2015 for the BBC Proms season, <a href="http://www.bbc.co.uk/radio1" target="_new">Radio 1</a> celebrated their 20 year association with the island of Ibiza by enlisting the <a href="http://www.theheritageorchestra.com/" target="_new">Heritage Orchestra</a> to play over twenty classic dance club tracks for their first ever Prom, turning the Royal Albert Hall into one giant nightclub on a Wednesday night.</p>
+          <p>On July 29th 2015, for the BBC Proms season, <a href="http://www.bbc.co.uk/radio1" target="_new">Radio 1</a> celebrated their 20 year association with the island of Ibiza by enlisting the <a href="http://www.theheritageorchestra.com/" target="_new">Heritage Orchestra</a> to play over twenty classic dance club tracks for their first ever Prom, turning the Royal Albert Hall into one giant nightclub on a Wednesday night.</p>
           <p>Hit <strong>Play</strong> below to replay the concert, along with all the tweets published at the same time.</p>
           <button className={styles.startbutton} style={startButtonStyle} onClick={this.start}>
             <i className="fa fa-play-circle"></i>
           </button>
         </div>
 
-        <div style={{ visibility: this.state.hasStarted == true ? 'visible' : 'hidden' }}>
+        <div style={
+          {
+            width:      '55%',
+            visibility: this.state.hasStarted == true ? 'visible' : 'hidden'
+          }
+        }>
           <VideoPlayer/>
-          <h2 className={styles.track} style={trackInfoStyle}>{this.props.track.title}</h2>
-          <p className={styles.artist} style={trackInfoStyle}>{this.props.track.artist}</p>
+
+          <div className={styles.description}>
+            <p><i className="fa fa-info-circle"></i> This is an experimental hack to attempt to synchronise the Radio 1 Ibiza Prom with the tweets of the night, exactly as it happened. In addition, the colour scheme of the page changes to match the predominant colour in the playing video. It is only a proof of concept and may not always be on its best behaviour. Any comments, shoot them my way <a href="https://twitter.com/indeox" target="_new" style={highlightStyle}>@indeox</a></p>
+          </div>
+          {
+            /*<h2 className={styles.track} style={trackInfoStyle}>{this.props.track.title}</h2>
+            <p className={styles.artist} style={trackInfoStyle}>{this.props.track.artist}</p>*/
+          }
         </div>
       </div>
     );
