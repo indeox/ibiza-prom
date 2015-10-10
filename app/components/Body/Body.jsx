bede99c81525b280d716580a1a325cc102bb4da9
David Vella <dave@deepcobalt.com>
2015-10-10 16:59:25 +0200

    Updated copy text

--- app/components/Body/Body.jsx
+++ app/components/Body/Body.jsx
@@ -74,7 +74,8 @@ export default class Body extends React.Component {
 
         <div className={styles.intro} style={{ display: this.state.hasStarted == true ? 'none' : 'block' }}>
           <p>On July 29th 2015, for the BBC Proms season, <a href="http://www.bbc.co.uk/radio1" target="_new">Radio 1</a> celebrated their 20 year association with Ibiza by enlisting the <a href="http://www.theheritageorchestra.com/" target="_new">Heritage Orchestra</a> to play over twenty classic club tracks for their first night out at the Proms, turning the Royal Albert Hall into one giant nightclub and throwing the poshest rave in London.</p>
-          <p>Hit <strong>Play</strong> below to replay the concert, along with all the tweets published at the same time. <em>Best viewed on larger screens.</em></p>
+          <p>This is an experimental hack to replay the concert, in sync with all the tweets published on that evening.</p>
+          <p>Hit <strong>Play</strong> below to kick it off.</p>
 
           <button className={styles.startbutton} style={startButtonStyle} onClick={this.start}>
             <i className={startButtonClass}></i>
@@ -85,7 +86,8 @@ export default class Body extends React.Component {
           <VideoPlayer/>
 
           <div className={styles.description}>
-            <p><i className="fa fa-info-circle"></i> This is an experimental hack, attempting to synchronise the Radio 1 Ibiza Prom with the tweets of the night, exactly as they happened. In addition, the colour scheme of the page changes to match the predominant colour in the playing video. It is only a proof of concept and may not always be on its best behaviour. Any comments, fire them at <a href="https://twitter.com/indeox" target="_new" style={highlightStyle}>@indeox</a></p>
+            <p><i className="fa fa-info-circle"></i> This is an experimental hack, attempting to synchronise the Radio 1 Ibiza Prom with the tweets of the night, exactly as they happened. The colour scheme of the page also changes to match the predominant colour in the playing video. </p>
+            <p>It was made purely for the fun of it, and as a proof of concept, it may sometimes behave erratically. If all fails, you can still enjoy the fantastic gig for what it was. Any comments, fire them at <a href="https://twitter.com/indeox" target="_new" style={highlightStyle}>@indeox</a></p>
           </div>
           {
             /*<h2 className={styles.track} style={trackInfoStyle}>{this.props.track.title}</h2>
